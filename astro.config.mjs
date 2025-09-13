d163559c5d3a98f56b5540725a955c366ad3c14b
Pratish Neerputh <pratz.neerputh@gmail.com>
2025-09-13 07:17:46 +0200

    added base dir for gh repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    site: 'https://kyroninja.github.io'
+    site: 'https://kyroninja.github.io',
+    base: '/devportfolio'
   },
 });
